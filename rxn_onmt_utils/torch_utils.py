0cccd4ed9dced33974dbf6972e3eddc3e0f21050
Y9DY19848 <y9dy19848@nomail.relay.ibm.com>
2021-02-05 17:49:14 +0000

    [skip ci] rxn-addlicense: Updated license headers

--- rxn_onmt_utils/torch_utils.py
+++ rxn_onmt_utils/torch_utils.py
@@ -1,3 +1,8 @@
+# LICENSED INTERNAL CODE. PROPERTY OF IBM.
+# IBM Research Zurich Licensed Internal Code
+# (C) Copyright IBM Corp. 2021
+# ALL RIGHTS RESERVED
+
 def set_num_threads(n: int) -> None:
     """
     Set the number of threads to be used by torch.
--- rxn_onmt_utils/translator.py
+++ rxn_onmt_utils/translator.py
@@ -1,3 +1,8 @@
+# LICENSED INTERNAL CODE. PROPERTY OF IBM.
+# IBM Research Zurich Licensed Internal Code
+# (C) Copyright IBM Corp. 2021
+# ALL RIGHTS RESERVED
+
 from argparse import Namespace
 from typing import List, Iterable, Union, Optional, Any
 
