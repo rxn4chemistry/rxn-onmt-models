17c8919baad05fbc4eb8db7a7aa9460adc4bd8f1
Alain Vaucher <AVA@zurich.ibm.com>
2022-04-06 14:15:24 +0200

    Update package setup (#13) [bump minor]

--- setup.py
+++ setup.py
@@ -3,35 +3,6 @@
 # (C) Copyright IBM Corp. 2021
 # ALL RIGHTS RESERVED
 
-import io
-import re
+from setuptools import setup
 
-from setuptools import setup, find_packages
-
-match = re.search(
-    r'__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
-    io.open('rxn_onmt_utils/__init__.py', encoding='utf_8_sig').read()
-)
-if match is None:
-    raise SystemExit('Version number not found.')
-__version__ = match.group(1)
-
-setup(
-    name='rxn_onmt_utils',
-    version=__version__,
-    author='IBM RXN team',
-    packages=find_packages(),
-    package_data={'rxn_onmt_utils': ['py.typed']},
-    install_requires=[
-        'attrs>=19.1.0',
-        'click>=7.0',
-        'rxn-opennmt-py>=1.0.3',
-    ],
-    extras_require={
-        'dev':
-            [
-                'flake8>=3.8.4', 'mypy>=0.761', 'pytest>=5.3.4', 'yapf>=0.31.0',
-                'bump2version>=1.0.1'
-            ],
-    },
-)
+setup()
