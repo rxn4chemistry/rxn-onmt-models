c3be06089c326ef7c1c025e74845c7b767dc34e6
Alain Vaucher <AVA@zurich.ibm.com>
2023-01-05 17:16:17 +0100

    Refactor the script for getting the stats (#67)

--- rxn_onmt_utils/scripts/dataset_stats.py
+++ rxn_onmt_utils/scripts/dataset_stats.py
@@ -1,12 +1,13 @@
 import logging
 from pathlib import Path
-from typing import Iterator, List, Tuple
+from typing import Any, Callable, Dict, Iterator, List, Optional, Tuple
 
 import click
 import numpy as np
 import pandas as pd
+from attr import define
 from rxn.chemutils.miscellaneous import get_individual_compounds
-from rxn.chemutils.tokenization import detokenize_smiles
+from rxn.chemutils.tokenization import detokenize_smiles, to_tokens
 from rxn.utilities.files import iterate_lines_from_file
 from rxn.utilities.logging import setup_console_logger
 
@@ -15,6 +16,21 @@ from rxn_onmt_utils.rxn_models.utils import RxnPreprocessingFiles
 
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
+
+
+def number_compounds(smiles_list: List[str]) -> int:
+    return len(smiles_list)
+
+
+def number_characters(smiles_list: List[str]) -> int:
+    return len(".".join(smiles_list))
+
+
+def number_tokens(smiles_list: List[str]) -> Optional[int]:
+    try:
+        return len(to_tokens(".".join(smiles_list)))
+    except Exception:
+        return None
 
 
 def iter_groups_of_smiles(path: Path) -> Iterator[List[str]]:
@@ -28,6 +44,63 @@ def iter_groups_of_smiles(path: Path) -> Iterator[List[str]]:
             yield get_individual_compounds(line)
         except Exception as e:
             logger.error(f'Could not get the compounds for "{line}": {e}')
+
+
+def get_data_file_path(
+    src_or_tgt: str, data_files: RxnPreprocessingFiles, model_task: str, split: str
+) -> Path:
+    """
+    Get the filepath to the file with data (one item per reaction).
+
+    Args:
+        src_or_tgt: "src" or "tgt".
+        data_files: structure indicating the directory for the data.
+        model_task: model task.
+        split: "train", "validation", or "test".
+    """
+    if src_or_tgt == "src":
+        return data_files.get_src_file(model_task=model_task, split=split)
+    elif src_or_tgt == "tgt":
+        return data_files.get_tgt_file(model_task=model_task, split=split)
+    else:
+        raise ValueError(f'Invalid value: "{src_or_tgt}" (should be "src" or "tgt"')
+
+
+@define
+class NumberStat:
+    """Wrapper for a function to obtain a value for a given list of compounds,
+    associated with a name.
+
+    An example for the number of compounds could be: NumberStat("n_smiles", len).
+    """
+
+    prefix: str
+    fn: Callable[[List[str]], Optional[float]]
+
+
+def make_copy_and_fill(
+    results_base: Dict[str, str],
+    split: str,
+    values_lists: List[List[float]],
+    stats: List[NumberStat],
+) -> Dict[str, Any]:
+    """Extracted to avoid duplication.
+
+    Args:
+        results_base: "template" dictionary with global info on dataset and file.
+        split: which split is considered.
+        values_lists: lists of lists of values to add (one list per stat).
+        stats: which stats were generated.
+    """
+    results_dict = {**results_base, "split": split}
+    for values_list, stat in zip(values_lists, stats):
+        results_dict.update(
+            {
+                f"{stat.prefix}_mean": np.mean(values_list),
+                f"{stat.prefix}_std": np.std(values_list),
+            }
+        )
+    return results_dict
 
 
 @click.command()
@@ -47,6 +120,12 @@ def main(csv: str, model_task: str, directories: Tuple[str, ...]) -> None:
     """
     setup_console_logger()
 
+    stats = [
+        NumberStat("n_compounds", fn=number_compounds),
+        NumberStat("n_chars", fn=number_characters),
+        NumberStat("n_tokens", fn=number_tokens),
+    ]
+    n_stats = len(stats)
     splits = ["train", "validation", "test"]
 
     results_dicts = []
@@ -54,32 +133,57 @@ def main(csv: str, model_task: str, directories: Tuple[str, ...]) -> None:
         logger.info(f'Getting stats for dataset in "{directory}"')
         data_files = RxnPreprocessingFiles(directory)
 
-        for split in splits:
+        for src_or_tgt in ["src", "tgt"]:
+            # Create a list of empty lists to contain the aggregated values from all splits
+            stats_for_all: List[List[float]] = [[] for _ in range(n_stats)]
 
-            src = data_files.get_src_file(split, model_task)
-            tgt = data_files.get_tgt_file(split, model_task)
+            results_base = {
+                "dataset_path": str(data_files.processed_data_dir),
+                "dataset": data_files.processed_data_dir.name,
+                "set": src_or_tgt,
+            }
 
-            for tag, path in [("src", src), ("tgt", tgt)]:
-                list_of_n_compounds = []
-                list_of_n_chars = []
-                for smiles_group in iter_groups_of_smiles(path):
-                    list_of_n_compounds.append(len(smiles_group))
-                    list_of_n_chars.append(
-                        len(".".join(smiles_group))
-                    )  # Note: this is approximate
+            for split in splits:
 
-                results_dicts.append(
-                    {
-                        "dataset_path": str(data_files.processed_data_dir),
-                        "dataset": data_files.processed_data_dir.name,
-                        "set": tag,
-                        "split": split,
-                        "n_compounds_mean": np.mean(list_of_n_compounds),
-                        "n_compounds_std": np.std(list_of_n_compounds),
-                        "n_chars_mean": np.mean(list_of_n_chars),
-                        "n_chars_std": np.std(list_of_n_chars),
-                    }
+                path = get_data_file_path(
+                    src_or_tgt=src_or_tgt,
+                    data_files=data_files,
+                    model_task=model_task,
+                    split=split,
                 )
+
+                stats_for_split: List[List[float]] = [[] for _ in range(n_stats)]
+
+                # Compute all the stats, one line at a time
+                for smiles_group in iter_groups_of_smiles(path):
+                    for values_list, stat in zip(stats_for_split, stats):
+                        value = stat.fn(smiles_group)
+                        if value is not None:
+                            values_list.append(value)
+
+                # Add values for the split to the global one
+                for values, values_all in zip(stats_for_split, stats_for_all):
+                    values_all.extend(values)
+
+                # Add one row for the selected split
+                results_dicts.append(
+                    make_copy_and_fill(
+                        results_base,
+                        split=split,
+                        values_lists=stats_for_split,
+                        stats=stats,
+                    )
+                )
+
+            # Add one row aggregating all the splits
+            results_dicts.append(
+                make_copy_and_fill(
+                    results_base,
+                    split="all",
+                    values_lists=stats_for_all,
+                    stats=stats,
+                )
+            )
 
     df = pd.DataFrame(results_dicts)
 
