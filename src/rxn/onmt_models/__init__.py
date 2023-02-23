405dd131c56b1cf8c8cbebd6bc2d327729677f0f
Travis CI Bump version <rxnid@zurich.ibm.com>
2023-02-23 13:09:40 +0000

    Bump version 0.10.0 → 0.10.1 via TravisCI (31.2) from aec869e

--- src/rxn/onmt_models/__init__.py
+++ src/rxn/onmt_models/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.10.0"  # managed by bump2version
+__version__ = "0.10.1"  # managed by bump2version
 
 from rxn.onmt_models.prediction_collapser import PredictionCollapser
 from rxn.onmt_models.translation import rxn_translation
