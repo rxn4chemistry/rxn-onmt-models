3824859ea0969924b8638064f4f09ee75c450dba
Alain Vaucher <ava@zurich.ibm.com>
2023-02-23 14:02:26 +0100

    Add imports in __init__.py

--- src/rxn/onmt_models/__init__.py
+++ src/rxn/onmt_models/__init__.py
@@ -1,1 +1,9 @@
 __version__ = "0.9.1"  # managed by bump2version
+
+from rxn.onmt_models.prediction_collapser import PredictionCollapser
+from rxn.onmt_models.translation import rxn_translation
+
+__all__ = [
+    "rxn_translation",
+    "PredictionCollapser",
+]
